54ce98f5be9d9f2538ee0680668d5ce20cf7c6e5
Adrian Gómez Reyes <adrigoreyes@gmail.com>
2020-11-20 05:08:42 +0100

    css

--- controlador.js
+++ controlador.js
@@ -2,16 +2,31 @@
     el: '#app',
     data: {
       mensaje: 'holaasd',
-      mensajes: ['wii']},
+      mensajes: []},
       methods:{
+       mm: function(){
+         this.mensaje = '';
+       },
         update: function(){
           fetch('https://api.telegram.org/bot1226812462:AAFBjHsd3NJtRwXt304buRYfeW-0t_OGi7Q/getUpdates')
           .then(response => response.json())
          .then( json => {
            let datos = json.result;
            const lista = [];
+           let item = {};
            for(let key in datos){
-             lista.push(datos[key].message.text);
+            /* item.text = datos[key].message.text
+             item.name =  datos[key].message.from.first_name;
+             item.key =  datos[key].update_id;
+             item.from_id = datos[key].message.from.id;*/
+             lista.push({
+               text: datos[key].message.text,
+               name: datos[key].message.from.first_name,
+               key: datos[key].update_id,
+               from_id: datos[key].message.from.id
+
+
+            });
              console.log(datos[key]);
            }
            this.mensajes = lista;
@@ -26,7 +41,7 @@
          
           var url = 'https://api.telegram.org/bot1226812462:AAFBjHsd3NJtRwXt304buRYfeW-0t_OGi7Q/sendMessage';
           data = {
-            "chat_id": 877561784,
+            "chat_id": 837689725,
             "text": this.mensaje,
             }
 
